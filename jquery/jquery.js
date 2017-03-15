99b47219cdf6dcfc6f53b06319b837e8891d6263
yovannanetwork <yovannanetwork@gmail.com>
2017-03-15 09:20:56 -0500

    enviando al servidor

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -17,17 +17,19 @@ function init(){
 	$('#list-objet').on('click', '.btn', clickBotonItemJuego);
 }
 // funcion para ir a la siguiente seccion a travez de um
-function clickBotonItemJuego(_idGame){
+function clickBotonItemJuego(){
 	//console.log($(this).parent().data(_idGame));
-	var idGame = $(this).parent().data(_idGame);
+	var idGame = $(this).parent().data('id');
 	irNextSection('comentarios');
-	getComentarios(_idGame);
+	getComentarios(idGame);
 	idActualJuego = idGame;
 }
 function clickBtnComment(){
-	var name = $('#name-comment').val();
-	var commentText = $('#area-comment').val();
-	postComment(idActualJuego, name, commentText);
+	var name = $('#name-comment');
+	var commentText = $('#area-comment');
+	postComment(idActualJuego, name.val(), commentText.val());
+	name.val().focus();
+	name.val()=  "";
 }
 function postComment(_idGame, _name, _content){
 	$.ajax({
@@ -37,7 +39,7 @@ function postComment(_idGame, _name, _content){
 	}).success(function(_data){
 		console.log(_data)
 		getComentarios(_idGame);
-	})
+	});
 }
 function irNextSection(_idVisible){
 	var visible = $('.visible');
@@ -83,13 +85,19 @@ function getHistorial(){
 		dibujarHistorial(_data);
 	})
 }
+function getSinglePage(){
+	$.ajax({
+		url: '',
+		type: 'GET'
+	})
+}
 function getComentarios(_idGame){
 	$.ajax({
 		url: 'http://test-ta.herokuapp.com/games/'+_idGame+'/comments',
 		type: 'GET'
-	}).success(function(_datos){
+	}).success(function(_data){
 		//console.log(_datos);
-		dibujarComentario(_datos);
+		dibujarComentario(_data);
 	})
 }
 // create a game 
@@ -105,13 +113,13 @@ function getNewGame(_winner, _loser, _turn){
 function dibujarHistorial(_datos){
 	var listaGame = $('#list-objet');
 	for(var i in _datos){
-		var html = '<li class="list-group-item">'+_datos[i].winner_player+' gano a '+_datos[i].loser_player+' en '+_datos[i].number_of_turns_to_win+' movimientos '+'<button class="btn">comentar</button>';
+		var html = '<li data-id="'+_datos[i].id+'" class="list-group-item">'+_datos[i].winner_player+' gano a '+_datos[i].loser_player+' en '+_datos[i].number_of_turns_to_win+' movimientos '+'<button class="btn">comentar</button>';
 		listaGame.append(html);
 	}
 }
 function dibujarComentario(_datos){
 	var listComment = $('#lista-comment');
-	for(var i in _comment){
+	for(var i in _datos){
 		var html = '<li class="list-group-item">'+_datos[i].name +' dice:<p>'+_datos[i].content+'</p></li>';
 		listComment.append(html);
 	}
