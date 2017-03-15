40c87bda3d8feaf463651f6a48178a45263d8a90
yovannanetwork <yovannanetwork@gmail.com>
2017-03-15 10:40:51 -0500

    modification2

--- jquery/game.js
+++ jquery/game.js
@@ -13,26 +13,7 @@ function init(){
 	//singleGame();
 	//$('#enviar-comment').on('click', sendComment);
 }
-/*function getSingleGame(){
-	var idJuego = $(this).parent().data('idGame');
-	//console.log(idJuego);
-	getComment(idJuego);
-	idAtualGame = idJuego;
-	//singleGame();
-}
-function sendComment(){
-	//alert('jcbjdbc');
-	postComment(idAtualGame, $('#name-comment').val(), $('#area-comment').val());
-}
-function postComment(_idGame, _name, _content){
-	$.ajax({
-		url: 'http://test-ta.herokuapp.com/games/'+ _idGame+'/comments',
-		type: 'POST',
-		data: {comment: {name: _name, content: _content, idegame: _idGame}}
-	}).done(function(){
-		getComment(_idGame)
-	});
-}*/
+
 function winningConbination(_texto){
 	if(
 		(tabla[0] == _texto && tabla[1]==_texto && tabla[2]== _texto) || (tabla[3] == _texto && tabla[4]==_texto && tabla[5]== _texto) || (tabla[6] == _texto && tabla[7]==_texto && tabla[8]== _texto) || (tabla[0] == _texto && tabla[3]==_texto && tabla[6]== _texto) || (tabla[1] == _texto && tabla[4]==_texto && tabla[7]== _texto) || ( tabla[2] == _texto && tabla[5]==_texto && tabla[8]== _texto) || (tabla[0] == _texto && tabla[4]==_texto && tabla[8]== _texto) || (tabla[2] == _texto && tabla[4]==_texto && tabla[6]== _texto) )
@@ -76,54 +57,7 @@ function clearNewGame(){
 		$('#message').remove();
 	});
 }
-/*function getHistorial(){
-	$.ajax({
-		url: 'http://test-ta.herokuapp.com/games',
-		
-	}).done(function(_data){
-		//console.log(_data);
-		dibujarHistorial(_data);
-	})
-}
-function singleGame(_idGame){
-	$.ajax({
-		url: 'http://test-ta.herokuapp.com/games/'+_idGame
-		
-	}).done(function(_dato){
-		//console.log(_dato);
-		dibujarHistorial(_dato);
-	});
-}
-function getComment(_idComment){
-	$.ajax({
-		url: 'http://test-ta.herokuapp.com/games/'+_idComment+'/comments',
-		type: 'GET'
-	}).success(function(_data){
-		//console.log(_data);
-		dibujarComentario(_data);
-	});
-}
 
-function dibujarHistorial(_datos){
-	var lista = $('#list-objet');
-	lista.html(_datos);
-	for(var i in _datos){
-		//console.log(_datos[i].id);
-		var html = '<li idGame="'+_datos[i].id+'" class="list-group-item">'+_datos[i].winner_player+' gano a '+_datos[i].loser_player+' en '+_datos[i].number_of_turns_to_win
-+' movimientos '+'<button class="btn">Comentar</button>'+'</li>';
-		lista.append(html);
-	}
-}
-
-function dibujarComentario(_dato){
-	var listComment = $('#lista-comment');
-	listComment.empty();
-	for(var i in _dato){
-		var html = '<li class="list-group-item">'+ _dato[i].name+' dice:'+'<p class="text-comment">'+ _dato[i].content+'</p></li>';
-		listComment.append(html);
-	}
-	
-}*/
 
 
 
